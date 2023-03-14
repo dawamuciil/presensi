3ca5d2bef1fbe879b9e81b3dc378fd470e535e8c
dawamuciil <dawamuciil@gmail.com>
2023-03-14 12:08:45 +0700

    Add first workflow

--- lib/app/modules/login/controllers/login_controller.dart
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -1,11 +1,13 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:presence/app/controllers/page_index_controller.dart';
 import 'package:presence/app/routes/app_pages.dart';
 import 'package:presence/app/widgets/dialog/custom_alert_dialog.dart';
 import 'package:presence/app/widgets/toast/custom_toast.dart';
 
 class LoginController extends GetxController {
+  final pageIndexController = Get.find<PageIndexController>();
   RxBool isLoading = false.obs;
   RxBool obsecureText = true.obs;
   TextEditingController emailC = TextEditingController();
@@ -13,29 +15,36 @@ class LoginController extends GetxController {
 
   FirebaseAuth auth = FirebaseAuth.instance;
 
+  checkDefaultPassword() {
+    if (passC.text == 'qwertyuiop')
+      Get.toNamed(Routes.NEW_PASSWORD);
+    else {
+      Get.offAllNamed(Routes.HOME);
+      pageIndexController.changePage(0);
+    }
+  }
+
   Future<void> login() async {
     if (emailC.text.isNotEmpty && passC.text.isNotEmpty) {
       isLoading.value = true;
       try {
-        UserCredential userCredential = await auth.signInWithEmailAndPassword(
-            email: emailC.text, password: passC.text);
+        final credential = await auth.signInWithEmailAndPassword(
+          email: emailC.text.trim(),
+          password: passC.text,
+        );
 
-        if (userCredential.user != null) {
-          if (userCredential.user!.emailVerified == true) {
+        if (credential.user != null) {
+          if (credential.user!.emailVerified) {
             isLoading.value = false;
-            if (passC.text == "Ssbpetrokimia") {
-              Get.offAllNamed(Routes.NEW_PASSWORD);
-            } else {
-              Get.offAllNamed(Routes.HOME);
-            }
+            checkDefaultPassword();
           } else {
             CustomAlertDialog.showPresenceAlert(
-              title: "Belum Verivikasi ",
-              message: "Kamu belum Melakukan Verivikasi Di email kamu ",
+              title: "Email not yet verified",
+              message: "Are you want to send email verification?",
               onCancel: () => Get.back(),
               onConfirm: () async {
                 try {
-                  await userCredential.user!.sendEmailVerification();
+                  await credential.user!.sendEmailVerification();
                   Get.back();
                   CustomToast.successToast(
                       "Success", "We've send email verification to your email");
